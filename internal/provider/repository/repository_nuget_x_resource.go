f67a77a7c3ef58093b43dce6516982e29ebca487
Paul Horton <phorton@sonatype.com>
2025-07-15 11:50:50 +0100

    feat: add resource `sonatyperepo_repository_nuget_*`
    Signed-off-by: Paul Horton <phorton@sonatype.com>

---
+++ internal/provider/repository/repository_nuget_x_resource.go
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package repository
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+
+	"terraform-provider-sonatyperepo/internal/provider/repository/format"
+)
+
+// NewRepositoryNugetHostedResource is a helper function to simplify the provider implementation.
+func NewRepositoryNugetHostedResource() resource.Resource {
+	return &repositoryResource{
+		RepositoryFormat: &format.NugetRepositoryFormatHosted{},
+		RepositoryType:   format.REPO_TYPE_HOSTED,
+	}
+}
+
+// NewRepositoryNugetProxyResource is a helper function to simplify the provider implementation.
+func NewRepositoryNugetProxyResource() resource.Resource {
+	return &repositoryResource{
+		RepositoryFormat: &format.NugetRepositoryFormatProxy{},
+		RepositoryType:   format.REPO_TYPE_PROXY,
+	}
+}
+
+// NewRepositoryNugetGroupResource is a helper function to simplify the provider implementation.
+func NewRepositoryNugetGroupResource() resource.Resource {
+	return &repositoryResource{
+		RepositoryFormat: &format.NugetRepositoryFormatGroup{},
+		RepositoryType:   format.REPO_TYPE_GROUP,
+	}
+}
