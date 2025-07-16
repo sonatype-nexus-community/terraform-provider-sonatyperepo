b9c6dd8606b7a90613d34480a15a160ca03618e5
Paul Horton <phorton@sonatype.com>
2025-07-16 11:40:49 +0100

    feat: add resources `sonatyperepo_repository_helm_*`
    Signed-off-by: Paul Horton <phorton@sonatype.com>

---
+++ internal/provider/model/repository_helm.go
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package model
+
+import (
+	"terraform-provider-sonatyperepo/internal/provider/common"
+
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	sonatyperepo "github.com/sonatype-nexus-community/nexus-repo-api-client-go/v3"
+)
+
+// Helm Hosted
+// ----------------------------------------
+type RepositoryHelmHostedModel struct {
+	RepositoryHostedModel
+}
+
+func (m *RepositoryHelmHostedModel) FromApiModel(api sonatyperepo.SimpleApiHostedRepository) {
+	m.mapSimpleApiHostedRepository(api)
+}
+
+func (m *RepositoryHelmHostedModel) ToApiCreateModel() sonatyperepo.HelmHostedRepositoryApiRequest {
+	apiModel := sonatyperepo.HelmHostedRepositoryApiRequest{
+		Name:    m.Name.ValueString(),
+		Online:  m.Online.ValueBool(),
+		Storage: sonatyperepo.HostedStorageAttributes{},
+		Component: &sonatyperepo.ComponentAttributes{
+			ProprietaryComponents: common.NewFalse(),
+		},
+		Cleanup: &sonatyperepo.CleanupPolicyAttributes{
+			PolicyNames: make([]string, 0),
+		},
+	}
+	m.Storage.MapToApi(&apiModel.Storage)
+	mapCleanupToApi(m.Cleanup, apiModel.Cleanup)
+	m.Component.MapToApi(apiModel.Component)
+	return apiModel
+}
+
+func (m *RepositoryHelmHostedModel) ToApiUpdateModel() sonatyperepo.HelmHostedRepositoryApiRequest {
+	return m.ToApiCreateModel()
+}
+
+// Helm Proxy
+// ----------------------------------------
+type RepositoryHelmProxyModel struct {
+	RepositoryProxyModel
+}
+
+func (m *RepositoryHelmProxyModel) FromApiModel(api sonatyperepo.SimpleApiProxyRepository) {
+	m.Name = types.StringPointerValue(api.Name)
+	m.Online = types.BoolValue(api.Online)
+	m.Url = types.StringPointerValue(api.Url)
+
+	// Cleanup
+	if api.Cleanup != nil && len(api.Cleanup.PolicyNames) > 0 {
+		m.Cleanup = NewRepositoryCleanupModel()
+		mapCleanupFromApi(api.Cleanup, m.Cleanup)
+	}
+
+	// Storage
+	m.Storage.MapFromApi(&api.Storage)
+
+	// Proxy Specific
+	m.Proxy.MapFromApi(&api.Proxy)
+	m.NegativeCache.MapFromApi(&api.NegativeCache)
+	m.HttpClient.MapFromApiHttpClientAttributes(&api.HttpClient)
+	m.RoutingRule = types.StringPointerValue(api.RoutingRuleName)
+	if api.Replication != nil {
+		m.Replication.MapFromApi(api.Replication)
+	}
+}
+
+func (m *RepositoryHelmProxyModel) ToApiCreateModel() sonatyperepo.HelmProxyRepositoryApiRequest {
+	apiModel := sonatyperepo.HelmProxyRepositoryApiRequest{
+		Name:    m.Name.ValueString(),
+		Online:  m.Online.ValueBool(),
+		Storage: sonatyperepo.StorageAttributes{},
+		Cleanup: &sonatyperepo.CleanupPolicyAttributes{
+			PolicyNames: make([]string, 0),
+		},
+	}
+	m.Storage.MapToApi(&apiModel.Storage)
+
+	if m.Cleanup != nil {
+		mapCleanupToApi(m.Cleanup, apiModel.Cleanup)
+	}
+
+	// Proxy Specific
+	apiModel.Proxy = sonatyperepo.ProxyAttributes{}
+	m.Proxy.MapToApi(&apiModel.Proxy)
+
+	apiModel.NegativeCache = sonatyperepo.NegativeCacheAttributes{}
+	m.NegativeCache.MapToApi(&apiModel.NegativeCache)
+
+	apiModel.HttpClient = sonatyperepo.HttpClientAttributes{}
+	m.HttpClient.MapToApiHttpClientAttributes(&apiModel.HttpClient)
+
+	if m.Replication != nil {
+		apiModel.Replication = &sonatyperepo.ReplicationAttributes{}
+		m.Replication.MapToApi(apiModel.Replication)
+	}
+
+	apiModel.RoutingRule = m.RoutingRule.ValueStringPointer()
+
+	return apiModel
+}
+
+func (m *RepositoryHelmProxyModel) ToApiUpdateModel() sonatyperepo.HelmProxyRepositoryApiRequest {
+	return m.ToApiCreateModel()
+}
