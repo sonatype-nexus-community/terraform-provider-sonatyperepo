00ac46569ef216add4f8ead238027d36e1af8716
Paul Horton <phorton@sonatype.com>
2025-07-17 08:15:45 +0100

    feat: add resources `sonatyperepo_repository_conan_*`
    Signed-off-by: Paul Horton <phorton@sonatype.com>

---
+++ internal/provider/model/repository_conan.go
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package model
+
+import (
+	"terraform-provider-sonatyperepo/internal/provider/common"
+
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	sonatyperepo "github.com/sonatype-nexus-community/nexus-repo-api-client-go/v3"
+)
+
+// Conan Hosted
+// ----------------------------------------
+type RepositorConanHostedModel struct {
+	RepositoryHostedModel
+}
+
+func (m *RepositorConanHostedModel) FromApiModel(api sonatyperepo.SimpleApiHostedRepository) {
+	m.mapSimpleApiHostedRepository(api)
+}
+
+func (m *RepositorConanHostedModel) ToApiCreateModel() sonatyperepo.ConanHostedRepositoryApiRequest {
+	apiModel := sonatyperepo.ConanHostedRepositoryApiRequest{
+		Name:    m.Name.ValueString(),
+		Online:  m.Online.ValueBool(),
+		Storage: sonatyperepo.HostedStorageAttributes{},
+		Component: &sonatyperepo.ComponentAttributes{
+			ProprietaryComponents: common.NewFalse(),
+		},
+		Cleanup: &sonatyperepo.CleanupPolicyAttributes{
+			PolicyNames: make([]string, 0),
+		},
+	}
+	m.Storage.MapToApi(&apiModel.Storage)
+	mapCleanupToApi(m.Cleanup, apiModel.Cleanup)
+	m.Component.MapToApi(apiModel.Component)
+	return apiModel
+}
+
+func (m *RepositorConanHostedModel) ToApiUpdateModel() sonatyperepo.ConanHostedRepositoryApiRequest {
+	return m.ToApiCreateModel()
+}
+
+// Conan Proxy
+// ----------------------------------------
+type RepositoryConanProxyModel struct {
+	RepositoryProxyModel
+	Conan *conanProxyAttributesModel `tfsdk:"conan"`
+}
+
+func (m *RepositoryConanProxyModel) FromApiModel(api sonatyperepo.SimpleApiProxyRepository) {
+	m.Name = types.StringPointerValue(api.Name)
+	m.Online = types.BoolValue(api.Online)
+	m.Url = types.StringPointerValue(api.Url)
+
+	// Cleanup
+	if api.Cleanup != nil && len(api.Cleanup.PolicyNames) > 0 {
+		m.Cleanup = NewRepositoryCleanupModel()
+		mapCleanupFromApi(api.Cleanup, m.Cleanup)
+	}
+
+	// Storage
+	m.Storage.MapFromApi(&api.Storage)
+
+	// Proxy Specific
+	m.Proxy.MapFromApi(&api.Proxy)
+	m.NegativeCache.MapFromApi(&api.NegativeCache)
+	m.HttpClient.MapFromApiHttpClientAttributes(&api.HttpClient)
+	m.RoutingRule = types.StringPointerValue(api.RoutingRuleName)
+	if api.Replication != nil {
+		m.Replication.MapFromApi(api.Replication)
+	}
+}
+
+func (m *RepositoryConanProxyModel) ToApiCreateModel() sonatyperepo.ConanProxyRepositoryApiRequest {
+	apiModel := sonatyperepo.ConanProxyRepositoryApiRequest{
+		Name:    m.Name.ValueString(),
+		Online:  m.Online.ValueBool(),
+		Storage: sonatyperepo.StorageAttributes{},
+		Cleanup: &sonatyperepo.CleanupPolicyAttributes{
+			PolicyNames: make([]string, 0),
+		},
+	}
+	m.Storage.MapToApi(&apiModel.Storage)
+
+	if m.Cleanup != nil {
+		mapCleanupToApi(m.Cleanup, apiModel.Cleanup)
+	}
+
+	// Proxy Specific
+	apiModel.Proxy = sonatyperepo.ProxyAttributes{}
+	m.Proxy.MapToApi(&apiModel.Proxy)
+
+	apiModel.NegativeCache = sonatyperepo.NegativeCacheAttributes{}
+	m.NegativeCache.MapToApi(&apiModel.NegativeCache)
+
+	apiModel.HttpClient = sonatyperepo.HttpClientAttributes{}
+	m.HttpClient.MapToApiHttpClientAttributes(&apiModel.HttpClient)
+
+	if m.Replication != nil {
+		apiModel.Replication = &sonatyperepo.ReplicationAttributes{}
+		m.Replication.MapToApi(apiModel.Replication)
+	}
+
+	apiModel.RoutingRule = m.RoutingRule.ValueStringPointer()
+
+	// Conan Specific
+	if m.Conan != nil {
+		apiModel.ConanProxy = &sonatyperepo.ConanProxyAttributes{}
+		m.Conan.MapToApi(apiModel.ConanProxy)
+	}
+
+	return apiModel
+}
+
+func (m *RepositoryConanProxyModel) ToApiUpdateModel() sonatyperepo.ConanProxyRepositoryApiRequest {
+	return m.ToApiCreateModel()
+}
+
+// Conan Group
+// ----------------------------------------
+type RepositoryConanGroupModel struct {
+	RepositoryGroupDeployModel
+}
+
+func (m *RepositoryConanGroupModel) FromApiModel(api sonatyperepo.SimpleApiGroupDeployRepository) {
+	m.Name = types.StringPointerValue(api.Name)
+	m.Online = types.BoolValue(api.Online)
+	m.Url = types.StringPointerValue(api.Url)
+	m.Storage.MapFromApi(&api.Storage)
+	m.Group.MapFromApi(&api.Group)
+}
+
+func (m *RepositoryConanGroupModel) ToApiCreateModel() sonatyperepo.ConanGroupRepositoryApiRequest {
+	apiModel := sonatyperepo.ConanGroupRepositoryApiRequest{
+		Name:    m.Name.ValueString(),
+		Online:  m.Online.ValueBool(),
+		Storage: sonatyperepo.StorageAttributes{},
+	}
+	m.Storage.MapToApi(&apiModel.Storage)
+	m.Group.MapToApi(&apiModel.Group)
+	return apiModel
+}
+
+func (m *RepositoryConanGroupModel) ToApiUpdateModel() sonatyperepo.ConanGroupRepositoryApiRequest {
+	return m.ToApiCreateModel()
+}
+
+// conanProxyAttributesModel
+type conanProxyAttributesModel struct {
+	ConanVersion types.String `tfsdk:"conan_version"`
+}
+
+func (m *conanProxyAttributesModel) MapFromApi(api *sonatyperepo.ConanProxyAttributes) {
+	m.ConanVersion = types.StringPointerValue(api.ConanVersion)
+}
+
+func (m *conanProxyAttributesModel) MapToApi(api *sonatyperepo.ConanProxyAttributes) {
+	api.ConanVersion = m.ConanVersion.ValueStringPointer()
+}
---
+++ internal/provider/repository/format/conan.go
@@ -0,0 +1,261 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package format
+
+import (
+	"context"
+	"maps"
+	"net/http"
+	"terraform-provider-sonatyperepo/internal/provider/common"
+	"terraform-provider-sonatyperepo/internal/provider/model"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	sonatyperepo "github.com/sonatype-nexus-community/nexus-repo-api-client-go/v3"
+)
+
+type ConanRepositoryFormat struct {
+	BaseRepositoryFormat
+}
+
+type ConanRepositoryFormatHosted struct {
+	ConanRepositoryFormat
+}
+
+type ConanRepositoryFormatProxy struct {
+	ConanRepositoryFormat
+}
+
+type ConanRepositoryFormatGroup struct {
+	ConanRepositoryFormat
+}
+
+// --------------------------------------------
+// Generic Conan Format Functions
+// --------------------------------------------
+func (f *ConanRepositoryFormat) GetKey() string {
+	return common.REPO_FORMAT_CONAN
+}
+
+func (f *ConanRepositoryFormat) GetResourceName(repoType RepositoryType) string {
+	return getResourceName(f.GetKey(), repoType)
+}
+
+// --------------------------------------------
+// Hosted Conan Format Functions
+// --------------------------------------------
+func (f *ConanRepositoryFormatHosted) DoCreateRequest(plan any, apiClient *sonatyperepo.APIClient, ctx context.Context) (*http.Response, error) {
+	// Cast to correct Plan Model Type
+	planModel := (plan).(model.RepositorConanHostedModel)
+
+	// Call API to Create
+	return apiClient.RepositoryManagementAPI.CreateConanHostedRepository(ctx).Body(planModel.ToApiCreateModel()).Execute()
+}
+
+func (f *ConanRepositoryFormatHosted) DoReadRequest(state any, apiClient *sonatyperepo.APIClient, ctx context.Context) (any, *http.Response, error) {
+	// Cast to correct State Model Type
+	stateModel := (state).(model.RepositorConanHostedModel)
+
+	// Call to API to Read
+	apiResponse, httpResponse, err := apiClient.RepositoryManagementAPI.GetConanHostedRepository(ctx, stateModel.Name.ValueString()).Execute()
+	return *apiResponse, httpResponse, err
+}
+
+func (f *ConanRepositoryFormatHosted) DoUpdateRequest(plan any, state any, apiClient *sonatyperepo.APIClient, ctx context.Context) (*http.Response, error) {
+	// Cast to correct Plan Model Type
+	planModel := (plan).(model.RepositorConanHostedModel)
+
+	// Cast to correct State Model Type
+	stateModel := (state).(model.RepositorConanHostedModel)
+
+	// Call API to Create
+	return apiClient.RepositoryManagementAPI.UpdateConanHostedRepository(ctx, stateModel.Name.ValueString()).Body(planModel.ToApiUpdateModel()).Execute()
+}
+
+func (f *ConanRepositoryFormatHosted) GetFormatSchemaAttributes() map[string]schema.Attribute {
+	return getCommonHostedSchemaAttributes()
+}
+
+func (f *ConanRepositoryFormatHosted) GetPlanAsModel(ctx context.Context, plan tfsdk.Plan) (any, diag.Diagnostics) {
+	var planModel model.RepositorConanHostedModel
+	return planModel, plan.Get(ctx, &planModel)
+}
+
+func (f *ConanRepositoryFormatHosted) GetStateAsModel(ctx context.Context, state tfsdk.State) (any, diag.Diagnostics) {
+	var stateModel model.RepositorConanHostedModel
+	return stateModel, state.Get(ctx, &stateModel)
+}
+
+func (f *ConanRepositoryFormatHosted) UpdatePlanForState(plan any) any {
+	var planModel = (plan).(model.RepositorConanHostedModel)
+	planModel.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+	return planModel
+}
+
+func (f *ConanRepositoryFormatHosted) UpdateStateFromApi(state any, api any) any {
+	stateModel := (state).(model.RepositorConanHostedModel)
+	stateModel.FromApiModel((api).(sonatyperepo.SimpleApiHostedRepository))
+	return stateModel
+}
+
+// --------------------------------------------
+// PROXY Conan Format Functions
+// --------------------------------------------
+func (f *ConanRepositoryFormatProxy) DoCreateRequest(plan any, apiClient *sonatyperepo.APIClient, ctx context.Context) (*http.Response, error) {
+	// Cast to correct Plan Model Type
+	planModel := (plan).(model.RepositoryConanProxyModel)
+
+	// Call API to Create
+	return apiClient.RepositoryManagementAPI.CreateConanProxyRepository(ctx).Body(planModel.ToApiCreateModel()).Execute()
+}
+
+func (f *ConanRepositoryFormatProxy) DoReadRequest(state any, apiClient *sonatyperepo.APIClient, ctx context.Context) (any, *http.Response, error) {
+	// Cast to correct State Model Type
+	stateModel := (state).(model.RepositoryConanProxyModel)
+
+	// Call to API to Read
+	apiResponse, httpResponse, err := apiClient.RepositoryManagementAPI.GetConanProxyRepository(ctx, stateModel.Name.ValueString()).Execute()
+	return *apiResponse, httpResponse, err
+}
+
+func (f *ConanRepositoryFormatProxy) DoUpdateRequest(plan any, state any, apiClient *sonatyperepo.APIClient, ctx context.Context) (*http.Response, error) {
+	// Cast to correct Plan Model Type
+	planModel := (plan).(model.RepositoryConanProxyModel)
+
+	// Cast to correct State Model Type
+	stateModel := (state).(model.RepositoryConanProxyModel)
+
+	// Call API to Create
+	return apiClient.RepositoryManagementAPI.UpdateConanProxyRepository(ctx, stateModel.Name.ValueString()).Body(planModel.ToApiUpdateModel()).Execute()
+}
+
+func (f *ConanRepositoryFormatProxy) GetFormatSchemaAttributes() map[string]schema.Attribute {
+	additionalAttributes := getCommonProxySchemaAttributes()
+	maps.Copy(additionalAttributes, getConanProxySchemaAttributes())
+	return additionalAttributes
+}
+
+func (f *ConanRepositoryFormatProxy) GetPlanAsModel(ctx context.Context, plan tfsdk.Plan) (any, diag.Diagnostics) {
+	var planModel model.RepositoryConanProxyModel
+	return planModel, plan.Get(ctx, &planModel)
+}
+
+func (f *ConanRepositoryFormatProxy) GetStateAsModel(ctx context.Context, state tfsdk.State) (any, diag.Diagnostics) {
+	var stateModel model.RepositoryConanProxyModel
+	return stateModel, state.Get(ctx, &stateModel)
+}
+
+func (f *ConanRepositoryFormatProxy) UpdatePlanForState(plan any) any {
+	var planModel = (plan).(model.RepositoryConanProxyModel)
+	planModel.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+	return planModel
+}
+
+func (f *ConanRepositoryFormatProxy) UpdateStateFromApi(state any, api any) any {
+	stateModel := (state).(model.RepositoryConanProxyModel)
+	stateModel.FromApiModel((api).(sonatyperepo.SimpleApiProxyRepository))
+	return stateModel
+}
+
+// --------------------------------------------
+// GORUP Conan Format Functions
+// --------------------------------------------
+func (f *ConanRepositoryFormatGroup) DoCreateRequest(plan any, apiClient *sonatyperepo.APIClient, ctx context.Context) (*http.Response, error) {
+	// Cast to correct Plan Model Type
+	planModel := (plan).(model.RepositoryConanGroupModel)
+
+	// Call API to Create
+	return apiClient.RepositoryManagementAPI.CreateConanGroupRepository(ctx).Body(planModel.ToApiCreateModel()).Execute()
+}
+
+func (f *ConanRepositoryFormatGroup) DoReadRequest(state any, apiClient *sonatyperepo.APIClient, ctx context.Context) (any, *http.Response, error) {
+	// Cast to correct State Model Type
+	stateModel := (state).(model.RepositoryConanGroupModel)
+
+	// Call to API to Read
+	apiResponse, httpResponse, err := apiClient.RepositoryManagementAPI.GetConanGroupRepository(ctx, stateModel.Name.ValueString()).Execute()
+	return *apiResponse, httpResponse, err
+}
+
+func (f *ConanRepositoryFormatGroup) DoUpdateRequest(plan any, state any, apiClient *sonatyperepo.APIClient, ctx context.Context) (*http.Response, error) {
+	// Cast to correct Plan Model Type
+	planModel := (plan).(model.RepositoryConanGroupModel)
+
+	// Cast to correct State Model Type
+	stateModel := (state).(model.RepositoryConanGroupModel)
+
+	// Call API to Create
+	return apiClient.RepositoryManagementAPI.UpdateConanGroupRepository(ctx, stateModel.Name.ValueString()).Body(planModel.ToApiUpdateModel()).Execute()
+}
+
+func (f *ConanRepositoryFormatGroup) GetFormatSchemaAttributes() map[string]schema.Attribute {
+	return getCommonGroupSchemaAttributes(true)
+}
+
+func (f *ConanRepositoryFormatGroup) GetPlanAsModel(ctx context.Context, plan tfsdk.Plan) (any, diag.Diagnostics) {
+	var planModel model.RepositoryConanGroupModel
+	return planModel, plan.Get(ctx, &planModel)
+}
+
+func (f *ConanRepositoryFormatGroup) GetStateAsModel(ctx context.Context, state tfsdk.State) (any, diag.Diagnostics) {
+	var stateModel model.RepositoryConanGroupModel
+	return stateModel, state.Get(ctx, &stateModel)
+}
+
+func (f *ConanRepositoryFormatGroup) UpdatePlanForState(plan any) any {
+	var planModel = (plan).(model.RepositoryConanGroupModel)
+	planModel.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+	return planModel
+}
+
+func (f *ConanRepositoryFormatGroup) UpdateStateFromApi(state any, api any) any {
+	stateModel := (state).(model.RepositoryConanGroupModel)
+	stateModel.FromApiModel((api).(sonatyperepo.SimpleApiGroupDeployRepository))
+	return stateModel
+}
+
+// --------------------------------------------
+// Common Functions
+// --------------------------------------------
+func getConanProxySchemaAttributes() map[string]schema.Attribute {
+	return map[string]schema.Attribute{
+		"conan": schema.SingleNestedAttribute{
+			Description: "Conan Proxy specific configuration for this Repository",
+			Required:    true,
+			Attributes: map[string]schema.Attribute{
+				"conan_version": schema.StringAttribute{
+					Description: "Conan protocol version. Cannot be changed once repository is created.",
+					Required:    true,
+					Validators: []validator.String{
+						stringvalidator.OneOf(common.CONAN_PROTOCOL_V1, common.CONAN_PROTOCOL_V2),
+					},
+					PlanModifiers: []planmodifier.String{
+						stringplanmodifier.UseStateForUnknown(),
+					},
+				},
+			},
+		},
+	}
+}
