2a8029bfe1708437e34306eb6f107af0a13a7a32
Paul Horton <phorton@sonatype.com>
2025-10-14 07:39:56 +0100

    fix: `latest_policy` not included create response causing provider error for Docker Registry (#81)
    Signed-off-by: Paul Horton <phorton@sonatype.com>

--- internal/provider/repository/repository_common.go
+++ internal/provider/repository/repository_common.go
@@ -31,6 +31,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -381,6 +382,9 @@ func getHostedStandardSchema(repoFormat string, repoType format.RepositoryType) 
 			Optional:    true,
 			Computed:    true,
 			Default:     booldefault.StaticBool(false),
+			PlanModifiers: []planmodifier.Bool{
+				boolplanmodifier.UseStateForUnknown(),
+			},
 		}
 	}
 
