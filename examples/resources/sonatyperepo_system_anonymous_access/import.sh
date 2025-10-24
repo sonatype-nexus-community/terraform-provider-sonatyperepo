06492f14078755c3bb54cacef0980b80f7f1b374
Yohan Fougeray <yohan@euphoriaagency.com>
2025-10-24 15:57:37 +0200

    add comment in import example

--- examples/resources/sonatyperepo_system_anonymous_access/import.sh
+++ examples/resources/sonatyperepo_system_anonymous_access/import.sh
@@ -1,1 +1,6 @@
-terraform import sonatyperepo_system_anonymous_access.anonymous anonymous_access
+# Existing Anonymous Access configuraiton can be imported as follows.
+#
+# NOTE: The Identifier (ANONYMOUS_ACCESS) in below example has no meaning and is just to comply with Terraform syntax.
+
+# Example
+terraform import sonatyperepo_system_anonymous_access.anonymous ANONYMOUS_ACCESS
