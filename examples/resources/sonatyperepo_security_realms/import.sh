fdae37f7145676e36ff96b45848a8f9b04aeebf6
Yohan Fougeray <yohan@euphoriaagency.com>
2025-10-24 16:59:28 +0200

    update example and documentation

---
+++ examples/resources/sonatyperepo_security_realms/import.sh
@@ -0,0 +1,6 @@
+# Existing Security Realms configuration can be imported as follows.
+#
+# NOTE: The Identifier (SECURITY_REALMS) in below example has no meaning and is just to comply with Terraform syntax.
+
+# Example
+terraform import sonatyperepo_security_realms.realms SECURITY_REALMS
