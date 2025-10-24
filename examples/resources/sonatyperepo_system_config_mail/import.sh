30028933493fa28040e5a935fc222eae31aa6ad9
Yohan Fougeray <yohan@euphoriaagency.com>
2025-10-24 16:56:20 +0200

    update example and documentation

---
+++ examples/resources/sonatyperepo_system_config_mail/import.sh
@@ -0,0 +1,6 @@
+# Existing System Mail configuration can be imported as follows.
+#
+# NOTE: The Identifier (CONFIG_MAIL) in below example has no meaning and is just to comply with Terraform syntax.
+
+# Example
+terraform import sonatyperepo_system_config_mail.config_mail CONFIG_MAIL
