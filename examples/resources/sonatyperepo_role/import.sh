a8a1b28e545f7809ce7a17436f34cc09a139ce55
Yohan Fougeray <yohan@euphoriaagency.com>
2025-10-29 16:59:40 +0100

    add import and documentation

---
+++ examples/resources/sonatyperepo_role/import.sh
@@ -0,0 +1,6 @@
+# Existing proxy docker repository configuration can be imported as follows.
+#
+# NOTE: The Identifier ROLE_NAME needs to match role name in your sonatype nexus repository instance.
+
+# Example
+terraform import sonatyperepo_role.role_name ROLE_NAME
