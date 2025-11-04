775ad860f49722a9746a0714f0cabbdf8bc4ec0f
Paul Horton <phorton@sonatype.com>
2025-11-04 15:44:14 +0000

    feat: New resource to manage `baseurl` Capability
    Signed-off-by: Paul Horton <phorton@sonatype.com>

---
+++ internal/provider/capability/capability_common.go
@@ -0,0 +1,405 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package capability
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"reflect"
+	"slices"
+	capabilitytype "terraform-provider-sonatyperepo/internal/provider/capability/capability_type"
+	"terraform-provider-sonatyperepo/internal/provider/common"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	sonatyperepo "github.com/sonatype-nexus-community/nexus-repo-api-client-go/v3"
+	v3 "github.com/sonatype-nexus-community/nexus-repo-api-client-go/v3"
+)
+
+const (
+	CAPABILITY_ERROR_RESPONSE_PREFIX           = "Error response: "
+	CAPABILITY_GENERAL_ERROR_RESPONSE_GENERAL  = CAPABILITY_ERROR_RESPONSE_PREFIX + " %s"
+	CAPABILITY_GENERAL_ERROR_RESPONSE_WITH_ERR = CAPABILITY_ERROR_RESPONSE_PREFIX + " %s - %s"
+	CAPABILITY_ERROR_DID_NOT_EXIST             = "%s (ID=%s) Capability did not exist to %s"
+)
+
+// Generic to all Task Resources
+type capabilityResource struct {
+	common.BaseResource
+	CapabilityType capabilitytype.CapabilityTypeI
+}
+
+// Metadata returns the resource type name.
+func (c *capabilityResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = fmt.Sprintf("%s_%s", req.ProviderTypeName, c.CapabilityType.GetResourceName())
+}
+
+// Set Schema for this Resource
+func (c *capabilityResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = getCapabilitySchema(c.CapabilityType)
+}
+
+// This allows import of existing capabilities into Terraform state.
+func (c *capabilityResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	// Use the Capability ID as the import identifier
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
+
+func (c *capabilityResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	// Retrieve values from plan
+	plan, diags := c.CapabilityType.GetPlanAsModel(ctx, req.Plan)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		tflog.Error(ctx, fmt.Sprintf("Getting Plan data has errors: %v", resp.Diagnostics.Errors()))
+		return
+	}
+
+	// Request Context
+	ctx = context.WithValue(
+		ctx,
+		sonatyperepo.ContextBasicAuth,
+		c.Auth,
+	)
+
+	// Make API requet
+	capabilityCreateResponse, httpResponse, err := c.CapabilityType.DoCreateRequest(plan, c.Client, ctx, c.NxrmVersion)
+
+	// Handle Errors
+	if err != nil {
+		common.HandleApiError(
+			fmt.Sprintf("Error creating %s Capability", c.CapabilityType.GetType().String()),
+			&err,
+			httpResponse,
+			&resp.Diagnostics,
+		)
+		return
+	}
+	if !slices.Contains(c.CapabilityType.GetApiCreateSuccessResponseCodes(), httpResponse.StatusCode) {
+		common.HandleApiError(
+			fmt.Sprintf("Creation of %s Capability was not successful", c.CapabilityType.GetType().String()),
+			&err,
+			httpResponse,
+			&resp.Diagnostics,
+		)
+	}
+
+	plan = c.CapabilityType.UpdateStateFromApi(plan, capabilityCreateResponse)
+	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (c *capabilityResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	// Retrieve values from state
+	stateModel, diags := c.CapabilityType.GetStateAsModel(ctx, req.State)
+	resp.Diagnostics.Append(diags...)
+
+	// Handle any errors
+	if resp.Diagnostics.HasError() {
+		tflog.Error(ctx, fmt.Sprintf("Getting request data has errors: %v", resp.Diagnostics.Errors()))
+		return
+	}
+
+	// Set API Context
+	ctx = context.WithValue(
+		ctx,
+		sonatyperepo.ContextBasicAuth,
+		c.Auth,
+	)
+
+	// Make API Request
+	capabilityId, shouldReturn := getCapabilityIdFromState(stateModel, &resp.Diagnostics)
+	if shouldReturn {
+		return
+	}
+	capability, httpResponse, err := c.readCapabilityById(capabilityId.ValueString(), ctx)
+
+	// Handle any errors
+	if err != nil {
+		if httpResponse.StatusCode == http.StatusNotFound {
+			resp.State.RemoveResource(ctx)
+			common.HandleApiWarning(
+				fmt.Sprintf(CAPABILITY_ERROR_DID_NOT_EXIST, c.CapabilityType.GetKey(), capabilityId.ValueString(), "read"),
+				&err,
+				httpResponse,
+				&resp.Diagnostics,
+			)
+		} else {
+			common.HandleApiError(
+				fmt.Sprintf(CAPABILITY_ERROR_DID_NOT_EXIST, c.CapabilityType.GetKey(), capabilityId.ValueString(), "read"),
+				&err,
+				httpResponse,
+				&resp.Diagnostics,
+			)
+		}
+		return
+	}
+
+	if capability == nil {
+		resp.State.RemoveResource(ctx)
+		common.HandleApiWarning(
+			fmt.Sprintf(CAPABILITY_ERROR_DID_NOT_EXIST, c.CapabilityType.GetKey(), capabilityId.ValueString(), "read"),
+			&err,
+			httpResponse,
+			&resp.Diagnostics,
+		)
+		return
+	}
+
+	stateModel = c.CapabilityType.UpdateStateFromApi(stateModel, capability)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &stateModel)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (c *capabilityResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	// Retrieve values from plan
+	planModel, diags := c.CapabilityType.GetPlanAsModel(ctx, req.Plan)
+	resp.Diagnostics.Append(diags...)
+
+	// Retrieve values from state
+	stateModel, diags := c.CapabilityType.GetStateAsModel(ctx, req.State)
+	resp.Diagnostics.Append(diags...)
+
+	// Request Context
+	ctx = context.WithValue(
+		ctx,
+		sonatyperepo.ContextBasicAuth,
+		c.Auth,
+	)
+
+	// Make API requet
+	capabilityId, shouldReturn := getCapabilityIdFromState(stateModel, &resp.Diagnostics)
+	if shouldReturn {
+		return
+	}
+	httpResponse, err := c.CapabilityType.DoUpdateRequest(planModel, capabilityId.ValueString(), c.Client, ctx, c.NxrmVersion)
+
+	// Handle any errors
+	if err != nil {
+		if httpResponse.StatusCode == http.StatusNotFound {
+			resp.State.RemoveResource(ctx)
+			common.HandleApiWarning(
+				fmt.Sprintf(CAPABILITY_ERROR_DID_NOT_EXIST, c.CapabilityType.GetType().String(), capabilityId.ValueString(), "update"),
+				&err,
+				httpResponse,
+				&resp.Diagnostics,
+			)
+		} else {
+			common.HandleApiError(
+				fmt.Sprintf(CAPABILITY_ERROR_DID_NOT_EXIST, c.CapabilityType.GetType().String(), capabilityId.ValueString(), "update"),
+				&err,
+				httpResponse,
+				&resp.Diagnostics,
+			)
+		}
+		return
+	}
+
+	// Now Read from API
+	capability, httpResponse, err := c.readCapabilityById(capabilityId.ValueString(), ctx)
+
+	// Handle any errors
+	if err != nil {
+		if httpResponse.StatusCode == http.StatusNotFound {
+			resp.State.RemoveResource(ctx)
+			common.HandleApiWarning(
+				fmt.Sprintf(CAPABILITY_ERROR_DID_NOT_EXIST, c.CapabilityType.GetKey(), capabilityId.ValueString(), "update"),
+				&err,
+				httpResponse,
+				&resp.Diagnostics,
+			)
+		} else {
+			common.HandleApiError(
+				fmt.Sprintf(CAPABILITY_ERROR_DID_NOT_EXIST, c.CapabilityType.GetKey(), capabilityId.ValueString(), "update"),
+				&err,
+				httpResponse,
+				&resp.Diagnostics,
+			)
+		}
+		return
+	}
+
+	if capability == nil {
+		resp.State.RemoveResource(ctx)
+		common.HandleApiWarning(
+			fmt.Sprintf(CAPABILITY_ERROR_DID_NOT_EXIST, c.CapabilityType.GetKey(), capabilityId.ValueString(), "update"),
+			&err,
+			httpResponse,
+			&resp.Diagnostics,
+		)
+		return
+	}
+
+	stateModel = c.CapabilityType.UpdateStateFromApi(stateModel, capability)
+	resp.Diagnostics.Append(resp.State.Set(ctx, stateModel)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (c *capabilityResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	// Retrieve values from state
+	state, diags := c.CapabilityType.GetStateAsModel(ctx, req.State)
+	resp.Diagnostics.Append(diags...)
+
+	// Handle any errors
+	if resp.Diagnostics.HasError() {
+		tflog.Error(ctx, fmt.Sprintf("Getting state data has errors: %v", resp.Diagnostics.Errors()))
+		return
+	}
+
+	// Request Context
+	ctx = context.WithValue(
+		ctx,
+		sonatyperepo.ContextBasicAuth,
+		c.Auth,
+	)
+
+	// Make API request
+	capabilityId, shouldReturn := getCapabilityIdFromState(state, &resp.Diagnostics)
+	if shouldReturn {
+		return
+	}
+
+	attempts := 1
+	maxAttempts := 3
+	success := false
+
+	for !success && attempts < maxAttempts {
+		httpResponse, err := c.Client.CapabilitiesAPI.Delete4(ctx, capabilityId.ValueString()).Execute()
+
+		// Trap 500 Error as they occur when Repo is not in appropriate internal state
+		if httpResponse.StatusCode == http.StatusInternalServerError {
+			tflog.Info(ctx, fmt.Sprintf("Unexpected response when deleting Capability %s (attempt %d)", c.CapabilityType.GetType().String(), attempts))
+			attempts++
+			continue
+		}
+
+		if err != nil {
+			if httpResponse.StatusCode == http.StatusNotFound {
+				resp.State.RemoveResource(ctx)
+				resp.Diagnostics.AddWarning(
+					fmt.Sprintf(CAPABILITY_ERROR_DID_NOT_EXIST, c.CapabilityType.GetType().String(), capabilityId.ValueString(), "delete"),
+					fmt.Sprintf(CAPABILITY_GENERAL_ERROR_RESPONSE_GENERAL, httpResponse.Status),
+				)
+			} else {
+				resp.Diagnostics.AddError(
+					fmt.Sprintf(CAPABILITY_ERROR_DID_NOT_EXIST, c.CapabilityType.GetType().String(), capabilityId.ValueString(), "delete"),
+					fmt.Sprintf(CAPABILITY_GENERAL_ERROR_RESPONSE_WITH_ERR, httpResponse.Status, err),
+				)
+			}
+			return
+		}
+		if httpResponse.StatusCode != http.StatusNoContent {
+			resp.Diagnostics.AddError(
+				fmt.Sprintf("Unexpected response when deleting %s Capability (attempt %d)", c.CapabilityType.GetType().String(), attempts),
+				fmt.Sprintf("Error response: %s", httpResponse.Status),
+			)
+
+			time.Sleep(1 * time.Second)
+			attempts++
+		} else {
+			success = true
+		}
+	}
+}
+
+func (c *capabilityResource) readCapabilityById(capabilityId string, ctx context.Context) (*v3.CapabilityDTO, *http.Response, error) {
+	// Set API Context
+	ctx = context.WithValue(
+		ctx,
+		sonatyperepo.ContextBasicAuth,
+		c.Auth,
+	)
+
+	// Make API Request
+	apiResponse, httpResponse, err := c.Client.CapabilitiesAPI.List(ctx).Execute()
+
+	// Handle any errors
+	if err != nil {
+		return nil, httpResponse, err
+	}
+
+	// Find the actual Capability from the list returned
+	var capability *v3.CapabilityDTO
+	for _, cap := range apiResponse {
+		if cap.Id != nil && *cap.Id == capabilityId {
+			capability = &cap
+			break
+		}
+	}
+
+	return capability, httpResponse, nil
+}
+
+func getCapabilityIdFromState(state any, respDiags *diag.Diagnostics) (basetypes.StringValue, bool) {
+	capabilityIdStructField := reflect.Indirect(reflect.ValueOf(state)).FieldByName("Id").Interface()
+	capabilityId, ok := capabilityIdStructField.(basetypes.StringValue)
+	if !ok {
+		respDiags.AddError(
+			"Failed to determine Capability ID to delete from State",
+			fmt.Sprintf("%s %s", CAPABILITY_ERROR_RESPONSE_PREFIX, capabilityIdStructField),
+		)
+		return basetypes.StringValue{}, true
+	}
+	return capabilityId, false
+}
+
+func getCapabilitySchema(ct capabilitytype.CapabilityTypeI) schema.Schema {
+	return schema.Schema{
+		MarkdownDescription: ct.GetMarkdownDescription(),
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Description: "The internal ID of the Capability.",
+				Computed:    true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"notes": schema.StringAttribute{
+				Description: "Optional notes about configured capability.",
+				Optional:    true,
+			},
+			"enabled": schema.BoolAttribute{
+				Description: "Whether the Capability is enabled.",
+				Required:    true,
+				Optional:    false,
+			},
+			"properties": schema.SingleNestedAttribute{
+				Description: "Properties specific to this Capability type",
+				Required:    true,
+				Optional:    false,
+				Attributes:  ct.GetPropertiesSchema(),
+			},
+			"last_updated": schema.StringAttribute{
+				Computed: true,
+			},
+		},
+	}
+}
---
+++ internal/provider/model/capability.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package model
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	v3 "github.com/sonatype-nexus-community/nexus-repo-api-client-go/v3"
+)
+
+// Base Capability Model - used for create and update
+// ----------------------------------------
+type BaseCapabilityModel struct {
+	Id          types.String `tfsdk:"id"`
+	Notes       types.String `tfsdk:"notes"`
+	Enabled     types.Bool   `tfsdk:"enabled"`
+	LastUpdated types.String `tfsdk:"last_updated"`
+}
+
+func (m *BaseCapabilityModel) MapFromApi(api *v3.CapabilityDTO) {
+	m.Id = types.StringPointerValue(api.Id)
+	m.Notes = types.StringPointerValue(api.Notes)
+	m.Enabled = types.BoolPointerValue(api.Enabled)
+}
+
+func (m *BaseCapabilityModel) toApiCreateModel() *v3.CapabilityDTO {
+	api := v3.NewCapabilityDTOWithDefaults()
+	api.Notes = m.Notes.ValueStringPointer()
+	api.Enabled = m.Enabled.ValueBoolPointer()
+	return api
+}
+
+func (m *BaseCapabilityModel) toApiUpdateModel() *v3.CapabilityDTO {
+	api := v3.NewCapabilityDTOWithDefaults()
+	api.Id = m.Id.ValueStringPointer()
+	api.Notes = m.Notes.ValueStringPointer()
+	api.Enabled = m.Enabled.ValueBoolPointer()
+	return api
+}
