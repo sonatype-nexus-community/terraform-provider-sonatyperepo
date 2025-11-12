4f5bb5d9a7e3cd7672908be4676425c00adf1605
yfougeray-euphoria <59951800+yfougeray-euphoria@users.noreply.github.com>
2025-11-12 07:37:04 +0100

    feat: Support import for `sonatyperepo_repository_conda_*` resources (#172)
    * hotfix: docker repository import error
    
    * simplify repository common for import and add docker import test
    
    * use common constant for errors
    
    * handle import for conda repositories
    
    * follow docker repositories pattern
    
    * remove unnecessary code
    
    * fix import for conda proxy

---
+++ examples/resources/sonatyperepo_repository_conda_proxy/import.sh
@@ -0,0 +1,6 @@
+# Existing proxy conda repository configuration can be imported as follows.
+#
+# NOTE: The Identifier REPOSITORY_NAME needs to match repository name in your sonatype nexus repository instance.
+
+# Example
+terraform import sonatyperepo_repository_conda_proxy.example REPOSITORY_NAME
