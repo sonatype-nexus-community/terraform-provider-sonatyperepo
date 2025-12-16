f6f66ab501fd9626e791959b337f39b0c6af3eb2
Paul Horton <phorton@sonatype.com>
2025-12-16 10:00:39 +0000

    sonarqube finding
    Signed-off-by: Paul Horton <phorton@sonatype.com>

--- internal/provider/blob_store/blob_store_common_test.go
+++ internal/provider/blob_store/blob_store_common_test.go
@@ -31,6 +31,8 @@ const (
 	RES_ATTR_MEMBERS_COUNT    string = "members.#"
 	RES_ATTR_LAST_UPDATED     string = "last_updated"
 
+	RES_NAME_FMT string = "%s.test"
+
 	RES_TYPE_BLOB_STORE_FILE  string = "sonatyperepo_blob_store_file"
 	RES_TYPE_BLOB_STORE_GCS   string = "sonatyperepo_blob_store_gcs"
 	RES_TYPE_BLOB_STORE_GROUP string = "sonatyperepo_blob_store_group"
@@ -38,8 +40,8 @@ const (
 )
 
 var (
-	RES_NAME_BLOB_STORE_FILE  string = fmt.Sprintf("%s.test", RES_TYPE_BLOB_STORE_FILE)
-	RES_NAME_BLOB_STORE_GCS   string = fmt.Sprintf("%s.test", RES_TYPE_BLOB_STORE_GCS)
-	RES_NAME_BLOB_STORE_GROUP string = fmt.Sprintf("%s.test", RES_TYPE_BLOB_STORE_GROUP)
-	RES_NAME_BLOB_STORE_S3    string = fmt.Sprintf("%s.test", RES_TYPE_BLOB_STORE_S3)
+	RES_NAME_BLOB_STORE_FILE  string = fmt.Sprintf(RES_NAME_FMT, RES_TYPE_BLOB_STORE_FILE)
+	RES_NAME_BLOB_STORE_GCS   string = fmt.Sprintf(RES_NAME_FMT, RES_TYPE_BLOB_STORE_GCS)
+	RES_NAME_BLOB_STORE_GROUP string = fmt.Sprintf(RES_NAME_FMT, RES_TYPE_BLOB_STORE_GROUP)
+	RES_NAME_BLOB_STORE_S3    string = fmt.Sprintf(RES_NAME_FMT, RES_TYPE_BLOB_STORE_S3)
 )
