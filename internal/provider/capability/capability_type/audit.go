57ea7e880e8c94c8d967b609a7cb6b8502d43544
Paul Horton <phorton@sonatype.com>
2026-01-05 13:31:34 +0000

    fix: ID missing from Update (PUT) requests when updating Capabilities #251
    Signed-off-by: Paul Horton <phorton@sonatype.com>

--- internal/provider/capability/capability_type/audit.go
+++ internal/provider/capability/capability_type/audit.go
@@ -60,6 +60,7 @@ func (f *AuditCapability) DoCreateRequest(plan any, apiClient *v3.APIClient, ctx
 func (f *AuditCapability) DoUpdateRequest(plan any, capabilityId string, apiClient *v3.APIClient, ctx context.Context, version common.SystemVersion) (*http.Response, error) {
 	// Cast to correct Plan Model Type
 	planModel := (plan).(model.CapabilityAuditModel)
+	planModel.Id = types.StringValue(capabilityId)
 
 	// Call API to Update
 	return apiClient.CapabilitiesAPI.Update3(ctx, capabilityId).Body(*planModel.ToApiUpdateModel(version)).Execute()
